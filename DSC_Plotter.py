89e3ec0ea237aebbbb121ad24853a1c40acfe2d6
p3rAsperaAdAstra <60529579+p3rAsperaAdAstra@users.noreply.github.com>
2023-07-25 12:14:35 +0200

    v 0.9
    almost finished. already functional, but could use some finishing touches. then ready for an alpha? version.

--- DSC_Plotter.py
+++ DSC_Plotter.py
@@ -1,4 +1,5 @@
 import os 
+import re 
 import json
 import argparse
 import numpy as np
@@ -11,74 +12,111 @@ from matplotlib import gridspec
 from matplotlib import colormaps as cm
 
 # REQUIRES MATPLOTLIB 3.7.X !!!!
+# do not use \n[[a-zA-Z]\s]+\n type strings in the comment section of the file. The script will not be able to find all the data inside the "csv" file
 # add option to plot from cmd only providing dir.
+# add color selection options
+# create default dict like in other scripts.
 
-def file_reader(path):
 
-	'''Reads in a TRIOS output file for an SDT measurement from a path and returns the contained data as a dict.'''
+d_input = [file for file in os.listdir() if '-DSC' in file and os.path.splitext(file)[1] == '.txt'][0]
+d_ext = 'png'
+d_output = '.'.join((os.path.splitext(d_input)[0],d_ext))
+
+parser = argparse.ArgumentParser(description='Creates a plot of a Trios DSC outputfile, which has been exported to plaintext (.txt)')
+parser.add_argument('-i', '--input_file', default=d_input, help='Specify the input files.')
+parser.add_argument('-o', '--output_file', default=d_output, help='Specify the output files.')
+parser.add_argument('-x', '--extension', default=d_ext, help='Specify the output file\'s extension.')
+parser.add_argument('-s', '--silent', action='store_true', help='Run in silent mode. Do not open interactive')
+args = parser.parse_args()
+
+
+
+def get_data(path:str):
+
+	'''Reads in a TRIOS output file (export to .csv in trios software) for a DSC measurement. 
+	Input: Path (str)
+	Output: Data (dict).'''
+
+	def find_section_indices(string:str):
+
+		'''Searches the entire file string for the the section titles and returns their indices as
+		a list of tuples.'''
+
+		section_rex = r'\n\[[\w\s]+\]\n'
+		start_indices = []
+		end_indices = []
+		section_titles = []
+		num_steps = 0
+
+		for match in re.finditer(section_rex,string):
+			start_indices.append(match.start())
+			end_indices.append(match.end())
+			section_titles.append(match.group()[2:-2])
+
+			if match.group() == '\n[step]\n': num_steps += 1 # find out how many steps are in the file 
+
+		len_mismatch_msg = '''The amount of opening and closing angular brackets is not identical.
+		This means that the regex is not finding all the sections of the trios file.
+		Contact someone to fix this, or find out why section_rex is not capturing correctly.'''
+
+		assert len(start_indices) == len(end_indices) == len(section_titles), len_mismatch_msg
+
+		return start_indices,end_indices,section_titles,num_steps
+
+
 
 	with open(path,'r',encoding='utf8') as inf:
-		raw = inf.read()
+		raw = inf.read() # read in entire file as one string.
 
-	data = {}
+	raw = '\n[general]\n' + raw
 
-	indices_open = []
-	indices_close = []
-
-	for i,char in enumerate(raw):
-		if char == '[':
-			indices_open.append(i)
-		elif char == ']':
-			indices_close.append(i)
-
-	assert len(indices_open) == len(indices_close)
-
-	# handle obtaining general metadata here: index 0 to indices_open[0] 
-	prestring = raw[:indices_open[0]-1]
+	start_indices, end_indices, section_titles, num_steps = find_section_indices(raw) # find the indices of the different sections inside the file.
+	data = {} # this is where the data will end up.
+	data['meta'] = {}
+	data['steps'] = []
 	
-	data['meta'] = {line.split('\t')[0]:line.split('\t')[1] for line in prestring.split('\n')}
-
-	data['nums'] = []
+	# meta_string = raw[:start_indices[-num_steps]] # put meta data part of the string in a separate variable
+	# num_string = raw[start_indices[-num_steps]:]
+	# print(num_string)
 	
-	for i in range(len(indices_open)): # handle dynamic obtaining of meta data with a segment name here.
-		a,b = indices_open[i], indices_close[i]
-		try:
-			a2 = indices_open[i+1]
-		except IndexError:
-			a2 = len(raw)
+	# obtain the different meta data sections as dict.
+	for i in range(len(start_indices)-num_steps):
+		section_title = section_titles[i]
+		sec_start = start_indices[i] + len(section_title)
+		sec_end = start_indices[i+1]
+		section = raw[sec_start+4:sec_end]
+		
+		data['meta'][section_title] = {line.split('\t')[0]:line.split('\t')[1] for line in section.split('\n')}
+	
+	# obtain the different heat ramp numerical info
+	start_indices += [len(raw)] # add len of file so that slicing has a concrete end point.
+	for i in range(len(section_titles)):
+		if section_titles[i].strip().lower() == 'step':
+			step_data = {}
+			
+			sec_start = end_indices[i]
+			sec_end = start_indices[i+1]
+			section = raw[sec_start:sec_end-1]
+			lines = section.split('\n')
+			
+			# probs not very reliable for different kinds of out files.
+			step_data['program'] = lines[0]
+			step_data['variables'] = lines[1].split('\t')
+			step_data['units'] = lines[2].split('\t')
 
-		# test if the [step] header is used.
-		if raw[a:b+1] == '[step]':
-			num_data_dict = {}
-			# handle obtaining of numerical data headers here.
-			num_string_raw = raw[b+2:a2]
-
-			num_string_lines = [line for line in num_string_raw.split('\n')]
-			num_data_dict['program'] = num_string_lines[0]
-			num_data_dict['measurements'] = num_string_lines[1].split('\t')
-			num_data_dict['units'] = num_string_lines[2].split('\t')
-
-			# handle obtaining of numerical data here. after indices_close[-1].
-			num_data = [line.split('\t') for line in num_string_lines[3:]]
-			num_data = [[float(num.replace(',','.')) for num in tup] for tup in num_data if '' not in tup]
-			num_data_dict['num_data'] = num_data
-
-			data['nums'].append(num_data_dict)
-
-		else:
-			segment_name = raw[a+1:b]
-			segment_string = raw[b+2:a2-1]
-
-			fields = {line.split('\t')[0]:line.split('\t')[1] for line in segment_string.split('\n')}
-			data[segment_name] = fields
+			# get the numerical data. might have to do regex search in later release. 
+			step_data['num'] = [[float(num) for num in line.replace(',','.').split('\t') if num != ''] for line in lines[3:] if line != '']
+			
+			data['steps'].append(step_data)
 
 	return data
+	
 
 
 
-def json_writer(data,path,indent=4):
+def json_writer(data:dict,path:str,indent=4):
 
-	'''Write a dict (data) to a json file at (path).'''
+	'''Write the data (dict) to a json file. Location is specified by path (str).'''
 
 	with open(path,'w',encoding='utf8') as outf:
 		json.dump(data,outf,indent=indent,ensure_ascii=False)
@@ -86,23 +124,23 @@ def json_writer(data,path,indent=4):
 
 
 
-def get_key_index(alias,measurements):
+def get_key_index(alias:str,steps:list):
 
 	'''Finds the index of a certian variable (like Temperature) based on an alias from the data['measurements'] section
 	of the data input.'''
 
-	for i in range(len(measurements)):
-		if alias.strip().lower() in measurements[i].strip().lower():
+	for i in range(len(steps)):
+		if alias.strip().lower() in steps[i].strip().lower():
 			return i 
 
 
 
 
-def plot_SDT(data,outfile):
+def plot_SDT(data:dict,outfile:str):
 
-	# dynamically detect number of data series.
+	'''The actual plotting function. Takes the data (dict) from '''
 
-	def toggle_visibility(label):
+	def toggle_visibility(label:str):
 
 		'''Callback function for rax Checkbox.'''
 	
@@ -120,24 +158,26 @@ def plot_SDT(data,outfile):
 	rax = plt.subplot(gs[1]) 
 	gs.update(wspace=0.05)
 
-	for i in range(len(data['nums'])):
-		num_data = data['nums'][i]
-		measurements = num_data['measurements']
-		label = num_data['program']
+	for i in range(len(data['steps'])):
+		num_data = data['steps'][i]['num'] 
+		variables = data['steps'][i]['variables']
+		label = data['steps'][i]['program']
 
-		t_ind = get_key_index('time',measurements)
-		T_ind = get_key_index('temp',measurements)
-		HF_ind = get_key_index('heat',measurements)
+		num_data = [tup for tup in num_data if len(tup) == len(variables)] # remove incomplete entries in num data
 
-		t = np.array(num_data['num_data'])[:,t_ind]
-		T = np.array(num_data['num_data'])[:,T_ind]
-		HF = np.array(num_data['num_data'])[:,HF_ind]
+		t_ind = get_key_index('time',variables)
+		T_ind = get_key_index('temp',variables)
+		HF_ind = get_key_index('heat',variables)
 
-		t_unit = num_data['units'][t_ind]
-		T_unit = num_data['units'][T_ind]
-		HF_unit = num_data['units'][HF_ind]
+		t = np.array(num_data)[:,t_ind]
+		T = np.array(num_data)[:,T_ind]
+		HF = np.array(num_data)[:,HF_ind]
+
+		t_unit = variables[t_ind]
+		T_unit = variables[T_ind]
+		HF_unit = variables[HF_ind]
 		
-		ax.plot(T,HF,lw=0.7,label=label,alpha=0.5,color=cm.get_cmap('jet')(i/len(data['nums'])))
+		ax.plot(T,HF,lw=0.7,label=label,alpha=0.5) # add color controls
 		label_index_dict[str(i+1)+'.'+label] = i
 
 
@@ -160,8 +200,8 @@ def plot_SDT(data,outfile):
 
 	# here checkbox logic 
 	label_props = {'fontsize':[8]*4}
-	frame_props={'edgecolor': None}
-	check_props={'sizes':[200]*4,'color':[cm.get_cmap('jet')(i/len(data['nums'])) for i in range(len(data['nums']))]}
+	frame_props = {'edgecolor': [ax.lines[i].get_color() for i in range(len(ax.lines))]}
+	check_props = {'sizes':[200]*4,'color':[ax.lines[i].get_color() for i in range(len(ax.lines))]}
 	
 	checkbox = CheckButtons(rax, label_index_dict.keys(),
 		actives=[True]*len(ax.lines),
@@ -181,20 +221,6 @@ def plot_SDT(data,outfile):
 	
 
 
-d_input = [file for file in os.listdir() if '-DSC' in file and os.path.splitext(file)[1] == '.txt'][0]
-d_ext = 'png'
-d_output = '.'.join((os.path.splitext(d_input)[0],d_ext))
-
-parser = argparse.ArgumentParser(description='Creates a plot of a Trios DSC outputfile, which has been exported to plaintext (.txt)')
-parser.add_argument('-i', '--input_file', default=d_input, help='Specify the input files.')
-parser.add_argument('-o', '--output_file', default=d_output, help='Specify the output files.')
-parser.add_argument('-x', '--extension', default=d_ext, help='Specify the output file\'s extension.')
-parser.add_argument('-s', '--silent', action='store_true', help='Run in silent mode. Do not open interactive')
-args = parser.parse_args()
-
 infile = args.input_file
-outfile = '.'.join((os.path.splitext(infile)[0],args.extension))
-
-data = file_reader(infile)
-
-plot_SDT(data,outfile)
+data = get_data(infile)
+plot_SDT(data,args.output_file)
